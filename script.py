cad965647b325643c39868c57b995331efe70774
AhmedSharaf <a.moataz88@gmail.com>
2022-09-16 19:07:48 +0200

    Adding the computer guesser function

--- script.py
+++ script.py
@@ -13,5 +13,23 @@ def guess(x):
             print(f"Sorry, {guess} is too high guess again")
     print(f"You got it {guess} is just the number you are looking for")
 
+#Computer Guesser
+def computer_guesser(x):
+    low = 1
+    high = x
+    feedback = ''
+    guess = random.randint(low, high)
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)?').lower()
+        if feedback == 'h':
+            high = guess - 1
+        elif feedback == 'l':
+            low = guess + 1
+    print(f'The computer guessed {guess} correctly')
 #Calling the function
-guess(10)
+# guess(10)
+computer_guesser(10)
